170c3e16a962adbf6ee6a520e2861ed91909f962
tort <tartushkin00@yandex.ru>
2025-10-23 23:51:29 +0300

    iter4 : реализация пакета config

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/sirupsen/logrus"
 	cfg "github.com/tartushkin/TSHORT.git/cmd/config"
 	"github.com/tartushkin/TSHORT.git/internal/handler"
@@ -19,7 +19,7 @@ func main() {
 	h := handler.NewHandlers(sh)
 
 	go h.StartHTTP(ctx, cfg.Port) // запуск сервера
-	lg.Info("Listner", "Запущен http слушатель на порту ", cfg.Port)
+	lg.Info("Listner", fmt.Sprintf("Запущен http слушатель на порту %s", cfg.Port))
 
 	go func() {
 		<-ctx.Done()
