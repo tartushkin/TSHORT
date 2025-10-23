1c3e76002ee005f70ea4cec701f5e19b7ce7b8f9
tort <tartushkin00@yandex.ru>
2025-10-23 22:40:04 +0300

    iter4 : реализация пакета config

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -28,40 +28,11 @@ func (h *Handlers) postURLHandler(ctx echo.Context) error {
 
 	aliaseURL := h.Short.SetAliaseName(string(body))
 
-	shortURL := fmt.Sprintf("%s%s",h.Short.Address,aliaseURL)//h.Short.Address + aliaseURL
-	shortURL = strings.TrimSpace(h.Short.Address)
+	shortURL := fmt.Sprintf("%s%s",strings.TrimSpace(h.Short.Address),aliaseURL)
+	
 	return ctx.String(http.StatusCreated, shortURL)
 
 }
-
-//	func (h *Handlers) postHandler(w http.ResponseWriter, r *http.Request) {
-//		// Проверяем, что это POST-запрос
-//		if r.Method != http.MethodPost {
-//			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-//			return
-//		}
-//
-//		contentType := r.Header.Get("Content-Type")
-//		if contentType != "text/plain" {
-//			http.Error(w, "Content-Type must be text/plain", http.StatusBadRequest)
-//			return
-//		}
-//		body, err := io.ReadAll(r.Body)
-//		if err != nil {
-//			http.Error(w, "Возникал ошибка при чтении тела запроса", http.StatusBadRequest)
-//			return
-//		}
-//		defer r.Body.Close()
-//
-//		aliaseURL := h.Short.SetAliaseName(string(body))
-//
-//		shortURL := fmt.Sprintf("http://localhost:8080/%s", aliaseURL)
-//		w.Header().Set("Content-Type", "text/plain")
-//		w.WriteHeader(http.StatusCreated)
-//
-//		w.Write([]byte(shortURL))
-//
-// }
 
 func (h *Handlers) getRedirectHandler(ctx echo.Context) error {
 	if ctx.Request().Method != http.MethodGet {
@@ -82,22 +53,4 @@ func (h *Handlers) getRedirectHandler(ctx echo.Context) error {
 	return ctx.Redirect(http.StatusTemporaryRedirect, originalURL)
 }
 
-//func (h *Handlers) getHandler(w http.ResponseWriter, r *http.Request) {
-//
-//	if r.Method != http.MethodGet {
-//		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-//		return
-//	}
-//	parts := strings.Split(r.URL.Path, "/")
-//	if len(parts) < 2 {
-//		http.Error(w, "Not Found", http.StatusNotFound)
-//	}
-//	url, err := h.Short.GetAliasName(parts[1])
-//
-//	if err != nil {
-//		http.Error(w, "Error: "+err.Error(), http.StatusInternalServerError)
-//	}
-//	w.Header().Set("Location", url)
-//	w.WriteHeader(http.StatusTemporaryRedirect)
-//
-//}
+
--- internal/handler/post_test.go
+++ internal/handler/post_test.go
@@ -11,45 +11,6 @@ import (
 	"github.com/tartushkin/TSHORT.git/internal/service"
 )
 
-//func TestPostHandler(t *testing.T) {
-//	short := &service.Short{
-//		CacheURL: make(map[string]string),
-//	}
-//	handlers := &Handlers{Short: short}
-//
-//	//  1: Не POST-запрос
-//	req := httptest.NewRequest(http.MethodGet, "/", nil)
-//	w := httptest.NewRecorder()
-//	handlers.postHandler(w, req)
-//	if w.Code != http.StatusMethodNotAllowed {
-//		t.Errorf("Ожидали %d, получичли %d", http.StatusMethodNotAllowed, w.Code)
-//	}
-//
-//	//  2: Неправильный Content-Type
-//	req = httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString("https://example.com"))
-//	req.Header.Set("Content-Type", "application/json")
-//	w = httptest.NewRecorder()
-//	handlers.postHandler(w, req)
-//	if w.Code != http.StatusBadRequest {
-//		t.Errorf("Ожидали  %d, получли %d", http.StatusBadRequest, w.Code)
-//	}
-//
-//	// 3: Валидный запрос
-//	req = httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString("https://example.com"))
-//	req.Header.Set("Content-Type", "text/plain")
-//	w = httptest.NewRecorder()
-//	handlers.postHandler(w, req)
-//	if w.Code != http.StatusCreated {
-//		t.Errorf("Ожидали %d, получли %d", http.StatusCreated, w.Code)
-//	}
-//	if w.Header().Get("Content-Type") != "text/plain" {
-//		t.Errorf("Ожидали Content-Type %s, получили %s", "text/plain", w.Header().Get("Content-Type"))
-//	}
-//	// Проверяем, что ответ содержит сокращённый URL
-//	if len(w.Body.String()) == 0 {
-//		t.Error("Ожидали не пустой ответ")
-//	}
-//}
 
 func TestPostHandler(t *testing.T) {
 	// Инициализация
