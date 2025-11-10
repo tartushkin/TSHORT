efc137ec8df46e752852b918a7e1bbde48b21348
tort <tartushkin00@yandex.ru>
2025-11-10 23:27:05 +0300

    iter9 : fix

--- internal/handler/http_server.go
+++ internal/handler/http_server.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"context"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,23 +21,15 @@ func NewHandlers(short *service.Short) *Handlers {
 // StartHTTP - инициализация и запуск сервера
 func (h *Handlers) StartHTTP(ctx context.Context, httpPort string) error {
 	h.httpServer = echo.New()
-	h.httpServer.Use(middleware.Logger()) //в билиотеке уже есть middleware для логирования запрсов
-	h.httpServer.Use(middleware.Recover())
-	//h.httpServer.Use(middleware.Gzip()) //в билиотеке уже есть middleware для сжатия
-	h.httpServer.Use(middleware.GzipWithConfig(middleware.GzipConfig{
-		Skipper: func(c echo.Context) bool {
-			// Полностью запрещаем сжатие заголовка Location
-			if c.Response().Header().Get("Location") != "" {
-				return true
-			}
-			contentType := c.Request().Header.Get(echo.HeaderContentType)
-			return len(contentType) > 0 &&
-				(strings.HasPrefix(contentType, "text/") ||
-					strings.HasPrefix(contentType, "application/json"))
-		},
-		Level:     5,
-		MinLength: 15,
-	}))
+	h.httpServer.Use(
+		middleware.Logger(),  // Логирование
+		middleware.Recover(), // Обработка паник
+		avoidGzipOnLocation,  // Наш новый фильтр
+		middleware.GzipWithConfig(middleware.GzipConfig{
+			Level:     5,
+			MinLength: 15,
+		}), // Последний шаг - GZIP
+	)
 
 	h.httpServer.POST("/", h.oldPostURLHandler)
 	h.httpServer.GET("/:id", h.getRedirectHandler)
@@ -52,4 +43,18 @@ func (h *Handlers) StartHTTP(ctx context.Context, httpPort string) error {
 // остнавка http сервера
 func (h *Handlers) StopHTTP(ctx context.Context) {
 	h.httpServer.Shutdown(ctx)
+}
+
+func avoidGzipOnLocation(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Проверяем, есть ли заголовок Location
+		loc := c.Response().Header().Get("Location")
+		if loc != "" {
+			// Возвращаемся сразу, не позволяя middleware сжимать Location
+			return next(c)
+		}
+
+		// Остальные заголовки оставляем неизменными
+		return next(c)
+	}
 }
