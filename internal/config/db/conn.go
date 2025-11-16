0f18879d393eec8cb680b2fd35d1acc10e0e0ac7
tort <tartushkin00@yandex.ru>
2025-11-16 19:47:57 +0300

    iter10 : подлкючение БД

--- internal/config/db/conn.go
+++ internal/config/db/conn.go
@@ -8,20 +8,13 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-type repo struct {
-	conn *sql.DB
-}
-
 func NewConnection(ps string) (*sql.DB, error) {
-	repo := repo{}
 
 	db, err := sql.Open("pgx", ps)
 	if err != nil {
 		return nil, err
 	}
-	//defer db.Close()
 
-	repo.conn = db
 	//проверкаа подключения
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
