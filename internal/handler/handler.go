d9349479aff1fcbe0d8457bb191779249d40d3d3
tort <tartushkin00@yandex.ru>
2025-11-24 21:57:45 +0300

    iter13 : запись списка URL

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/tartushkin/TSHORT.git/internal/model"
@@ -65,6 +66,9 @@ func (h *Handlers) postURLHandler(ctx echo.Context) error {
 	listURL, err := h.Short.ReaderBody(ctx, model.One)
 	if err != nil {
 		res.ErrMsg = err.Error()
+		if strings.HasPrefix(res.ErrMsg, model.CONFLICT) {
+			return ctx.JSON(http.StatusConflict, res)
+		}
 		return ctx.JSON(http.StatusInternalServerError, res)
 	}
 	for _, couple := range listURL {
