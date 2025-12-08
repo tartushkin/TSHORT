20c586afb5386d032dd7f641d2f3fac5df050be0
tort <tartushkin00@yandex.ru>
2025-12-08 21:11:07 +0300

    iter15 : минус конфиги по умолчанию

--- internal/config/app/config.go
+++ internal/config/app/config.go
@@ -17,8 +17,8 @@ func NewConfig() *Config {
 	cfg := Config{}
 	flag.StringVar(&cfg.Port, "a", ":8080", "порт сервиса")
 	flag.StringVar(&cfg.Address, "b", "http://localhost:8080/", "базовый адрес результирующего сокращённого URL")
-	flag.StringVar(&cfg.FileStoragePath, "c", "./StorageURL.TXT", "путь для файла хранения URL")
-	flag.StringVar(&cfg.DNS, "d", "host=localhost port=5432 user=postgres password=12345678 dbname=myDB sslmode=disable", "cтрока с адресом подключения к БД")
+	//flag.StringVar(&cfg.FileStoragePath, "c", "./StorageURL.TXT", "путь для файла хранения URL")
+	//flag.StringVar(&cfg.DNS, "d", "host=localhost port=5432 user=postgres password=12345678 dbname=myDB sslmode=disable", "cтрока с адресом подключения к БД")
 
 	flag.Parse()
 
