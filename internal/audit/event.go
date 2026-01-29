28fb44a0bdafd87aa4a883a44153c88d51f4806b
tort <tartushkin00@yandex.ru>
2026-01-29 22:50:57 +0300

    iter16 : реализация аудита через наблюдателя

--- internal/audit/event.go
+++ internal/audit/event.go
@@ -4,7 +4,7 @@ import "time"
 
 type Event struct {
 	TS     int64  `json:"ts"`      // Unix timestamp
-	Action string `json:"action"`  // shorten/follow
+	Action string `json:"action"`  // shorten/followw
 	UserID string `json:"user_id"` // может быть пустым
 	URL    string `json:"url"`     // оригинальный URL
 }
