70634aac238cf6d5a4456bff4cd3a83d7eff1fd5
tort <tartushkin00@yandex.ru>
2026-02-03 22:58:44 +0300

    iter17 : профилирование

--- internal/handler/benchmark_test.go
+++ internal/handler/benchmark_test.go
@@ -18,7 +18,7 @@ func BenchmarkGetUserURLsHandler(b *testing.B) {
 	// 3. Валидный запрос
 	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBufferString("https://exampl.com"))
 	req.Header.Set("Content-Type", "text/plain")
-	rec := httptest.NewRecorder()
+	//rec := httptest.NewRecorder()
 	req.AddCookie(&http.Cookie{
 		Name:     "user_id",
 		Value:    "a7fa515e.fb64b57c209853ea0fb0fb0f2adb85aa6f1ef726d18c38c44d530036b5e7282f",
@@ -26,12 +26,12 @@ func BenchmarkGetUserURLsHandler(b *testing.B) {
 		HttpOnly: true,
 		Secure:   true, // Используйте true, если используете HTTPS
 	})
-	c := e.NewContext(req, rec)
+	//c := e.NewContext(req, rec)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		rec := httptest.NewRecorder()
-		c = e.NewContext(req, rec)
+		c := e.NewContext(req, rec)
 
 		_ = h.oldPostURLHandler(c)
 	}
